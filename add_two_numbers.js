5ac05937c1c2827eaa677d7d76823e4a6aa34aea
woo-sang-jeong <jeongwoosangdev@gmail.com>
2022-07-05 23:58:19 +0900

    blog template

--- add_two_numbers.js
+++ add_two_numbers.js
@@ -1,4 +1,4 @@
-function doSomething(){
+function doSomething() {
     let a = document.getElementById('inputA').value;
     let b = document.getElementById('inputB').value;
     document.getElementById("valueA").innerHTML = a;
--- what_time_is_it.js
+++ what_time_is_it.js
@@ -1,3 +1,3 @@
-function whatTimeIsIt(){
+function whatTimeIsIt() {
     alert(new Date());
 }
