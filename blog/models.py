54994bc01e11fa6bf1f87dd5d28686329de0ff95
woo-sang-jeong <jeongwoosangdev@gmail.com>
2022-07-11 20:47:21 +0900

    Post 모델생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #author: 나중에 추가
+
+    def __str__(self):
+        return f'[{self.pk}][{self.title}]'
