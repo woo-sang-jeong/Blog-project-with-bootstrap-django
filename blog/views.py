e13c97e1d0c0799555eed604f377648cc241bf11
woo-sang-jeong <jeongwoosangdev@gmail.com>
2022-07-14 22:02:27 +0900

    포스트 목록 페이지에 Bootstrap 적용

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
+# FBV 방식으로 제작할 때 필요 : from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
+
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+
+class PostDetail(DetailView):
+    model = Post
+
+
+"""
+FBV 방식으로 제작한 함수들
+
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-pk')
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post_list.html',
         {
             'posts': posts,
         }
     )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/post_detail.html',
+        {
+            'post' : post,
+        }
+    )
+"""
