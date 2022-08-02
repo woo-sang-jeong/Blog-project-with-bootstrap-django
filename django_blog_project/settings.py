0b52accab21229832fe5ed890c560e1f660a9977
woo-sang-jeong <jeongwoosangdev@gmail.com>
2022-08-02 23:23:01 +0900

    django_extensions, ipython 설치와 settings.py에 적용

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+
     'blog',
     'single_pages',
+
 ]
 
 MIDDLEWARE = [
