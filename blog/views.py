2ad1de9798d18b1df6387a3d1ec43e481c9da8d9
woo-sang-jeong <jeongwoosangdev@gmail.com>
2022-09-13 23:21:17 +0900

    pagination 기능 구현

--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,7 @@ from django.shortcuts import get_object_or_404
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
